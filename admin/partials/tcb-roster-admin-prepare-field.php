0e86483847c73bd43daf5b5960f8c31aad26d2f0
wjviant <wjviant@googlemail.com>
2022-10-24 07:58:24 +0100

    added logic to enable trainers to change profile
    WIP, as it exposes too much of the profile

--- admin/partials/tcb-roster-admin-prepare-field.php
+++ admin/partials/tcb-roster-admin-prepare-field.php
@@ -5,6 +5,11 @@ function tcb_roster_admin_prepare_field ( $field ) {
     if ( $field['_name'] == 'user-location' )
         return $field;
 
+    $user = wp_get_current_user();
+    if (($field['_name'] == 'courses_completed') && in_array("training_admin", $user->roles)) {
+        return $field;
+    }
+
     if ( ! current_user_can( 'manage_options' ) ) {
         return false;
     }
