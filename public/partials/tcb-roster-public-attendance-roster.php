60b6aa57ba88b121e9312b8b29f62d7b99d614ff
wjviant <wjviant@googlemail.com>
2022-12-02 14:00:29 +0000

    Added detailed briefing - WIP

--- public/partials/tcb-roster-public-attendance-roster.php
+++ public/partials/tcb-roster-public-attendance-roster.php
@@ -13,6 +13,74 @@ function tcb_roster_public_attendance_roster($attributes) {
 	if (!$post_id)
 		return;
 
+	// Output the briefing
+	echo '<div class="tcb_briefing" >';
+
+	echo '<h3>Situation</h3>';
+	echo get_field('situation');
+
+	echo '<h3>Mission</h3>';
+	echo get_field('mission');
+	
+	echo '<h3>Execution</h3>';
+	echo get_field('execution');
+	
+	echo '<h3>Intel</h3>';
+	echo get_field('intel');
+	
+	echo '<h3>Environment</h3>';
+	while( have_rows('environment') ): the_row();
+		echo '<h4>Map</h4>';
+		echo get_sub_field('map');
+		
+		echo '<h4>Terrain</h4>';
+		echo get_sub_field('terrain');
+		
+		echo '<h4>Time</h4>';
+		echo get_sub_field('time');
+		
+		echo '<h4>Weather</h4>';
+		echo get_sub_field('weather');
+		
+		echo '<h4>IED/Mine Threat</h4>';
+		echo get_sub_field('iedmine_threat');
+	endwhile;
+
+	echo '<h3>Enemy Forces</h3>';
+	echo get_field('enemy_forces');
+
+	echo '<h3>Friendly Forces</h3>';
+	echo get_field('friendly_forces');
+
+	echo '<h3>Civilians</h3>';
+	echo get_field('civilians');
+
+	echo '<h3>Service Support</h3>';
+	while( have_rows('service-support') ): the_row();
+		echo '<h4>Vehicles</h4>';
+		echo get_sub_field('vehicles');
+
+		echo '<h4>Supplies</h4>';
+		echo get_sub_field('supplies');
+
+		echo '<h4>Support</h4>';
+		echo get_sub_field('support');
+
+		echo '<h4>Reinforcements</h4>';
+		echo get_sub_field('reinforcements');
+	endwhile;
+
+	echo '<h3>Rules of Engagement</h3>';
+	echo get_field('rules_of_engagement');
+	
+	echo '<h3>Command and Signals</h3>';
+	echo get_field('command_and_signals');
+
+	echo '<h3>Modset</h3>';
+	echo get_field('modset');
+
+	echo '</div>';		
+
 	// Early out if no entrys in rsvp field
 	if(! have_rows('rsvp') )
 		return;		
