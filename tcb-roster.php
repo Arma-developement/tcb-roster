429f2f13fc6321d7f3ae455bd382893afa37ece7
wjviant <wjviant@googlemail.com>
2025-01-15 23:43:21 +0000

    Update tcb-roster.php

--- tcb-roster.php
+++ tcb-roster.php
@@ -3,7 +3,7 @@
 Plugin Name: 3CB Roster
 Plugin URI: https://github.com/Arma-developement/tcb-roster
 Description: Design by Nick at Intention and Lifetap
-Version: 1.3.8
+Version: 1.3.9
 Author: Lifetap / Badger
 Author URI: https://github.com/Arma-developement/tcb-roster
 License: GPL2
