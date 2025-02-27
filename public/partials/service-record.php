9ec838d99444a1c9857898d79599674023e101ba
wjviant <wjviant@googlemail.com>
2025-02-27 00:06:50 +0000

    Added status edit

--- public/partials/service-record.php
+++ public/partials/service-record.php
@@ -68,12 +68,14 @@ function tcbp_public_sr_form() {
 
 	echo '<div class="tcb_service_record_form">';
 
+	$term_ = get_term_by( 'name', 'Recruit', 'tcb-rank' );
+
 	acfe_form(
 		array(
 			'post_id'         => $post_id,
 			'field_groups'    => array( 'group_635697195a971', 'group_6356984d2ce21', 'group_6356980addb3c' ),
 			'map'             => array(
-				'field_67b8e1472f7bb' => array( 'value' => 50 ), // Rank - Recruit.
+				'field_67b8e1472f7bb' => array( 'value' => term_->id ), // Rank - Recruit.
 			),
 			'submit_value'    => 'Update ' . $display_name . "'s Service Record",
 			'updated_message' => false,
---
+++ public/partials/status.php
@@ -0,0 +1,50 @@
+<?php // phpcs:ignore Generic.Files.LineEndings.InvalidEOLChar
+/**
+ * File: status.php
+ * Description: Handles the code associated with the setting the status opf a post in the tcb plugin.
+ */
+
+add_shortcode( 'tcbp_public_edit_status', 'tcbp_public_edit_status' );
+
+/**
+ * Called buy a shortcode to display the status change form.
+ */
+function tcbp_public_edit_status() {
+
+	if ( ! isset( $_GET['id'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		return;
+	}
+
+	$post_id = sanitize_text_field( wp_unslash( $_GET['id'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+
+	if ( empty( $post_id ) ) {
+		return;
+	}
+
+	// Security check.
+	if ( ( '' !== $post_id ) && ( ! current_user_can( 'edit_post', $post_id ) ) ) {
+		return;
+	}
+
+	ob_start();
+
+	echo '<div class="tcb_edit_status">';
+
+	acf_form(
+		array(
+			'post_id'         => $post_id,
+			'field_groups'    => array( 'group_678bea513af25' ),
+			'submit_value'    => 'Update Status',
+			'return'          => wp_get_referer(),
+			'updated_message' => false,
+		)
+	);
+
+	echo '</div>';
+
+	if ( function_exists( 'SimpleLogger' ) ) {
+		SimpleLogger()->info( 'Edited the status of postID=' . $post_id );
+	}
+
+	return ob_get_clean();
+}
