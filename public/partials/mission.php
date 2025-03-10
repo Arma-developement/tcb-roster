cea51f98be58618abb07b7051b6c7142570a5005
Nick Hayward <info@fcswebsites.co.uk>
2025-03-10 22:55:52 +0000

    Update mission.php
    top items now a quarter not a third

--- public/partials/mission.php
+++ public/partials/mission.php
@@ -25,20 +25,21 @@ function tcbp_public_mission_overview() {
 
 	echo '<h2>Mission Details</h2>';
 
-	echo '<div class="container briefing-meta"><div class="one-third column"><h3>Author</h3>';
+	echo '<div class="container briefing-meta">';
+ 	echo '<div class="one-quarter column"><h3>Author</h3>';
 	$author_id = get_the_author_meta( 'ID' );
 	echo '<a href="/service-record/service-record-' . esc_attr( $author_id ) . '">' . esc_html( get_the_author_meta( 'display_name' ) ) . '</a>';
-	echo '<h3>Modset</h3>';
+	echo '</div>';
+	echo '<div class="one-quarter column"><h3>Modset</h3>';
 	echo get_field( 'brief_modset' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	echo '</div>';
-
-	echo '<div class="one-third column"><h3>Map</h3>';
+	echo '<div class="one-quarter column"><h3>Map</h3>';
 	echo get_field( 'brief_map' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	echo '</div>';
-
-	echo '<div class="one-third column"><h3>Time</h3>';
+	echo '<div class="one-quarter column"><h3>Time</h3>';
 	echo get_field( 'brief_start_time' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	echo '</div></div>';
+	echo '</div>';
+ 	echo '</div>';
 
 	echo '<h3>Situation</h3>';
 	echo get_field( 'brief_situation' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
