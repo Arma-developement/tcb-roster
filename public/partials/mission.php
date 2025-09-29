eb114388fdfa4cbfb63275894b09875c1dca75e6
wjviant <wjviant@googlemail.com>
2025-09-29 23:33:03 +0100

    Mission update

--- public/partials/mission.php
+++ public/partials/mission.php
@@ -58,12 +58,6 @@ function tcbp_public_mission_overview() {
 	$current_user_roles       = $current_user->roles;
 	$brief_mission_type_array = get_field( 'brief_mission_type', $post_id );
 	$brief_mission_type       = $brief_mission_type_array['value'];
-	echo '<pre>';
-	var_dump( $brief_mission_type_array );
-	echo '</pre>';
-	echo '<pre>';
-	var_dump( $brief_mission_type );
-	echo '</pre>';
 	if ( ( in_array( 'subscriber', $current_user_roles, true ) ) && in_array( $brief_mission_type, array( 'private', 'miniop', 'patrolop' ), true ) ) {
 		echo '<br><br><p>This is a 3CB members only mission</p>';
 		echo '<p>For information about 3CB, click <a href="/information-centre/about-3cb">here</a></p>';
