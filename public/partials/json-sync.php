a1567a27be3aa67f6e94612fdd8d90c9a55a4c88
Warren Viant <wjviant@users.noreply.github.com>
2026-07-22 16:23:43 +0100

    Update json-sync.php
    Removed json filters

--- public/partials/json-sync.php
+++ public/partials/json-sync.php
@@ -1,6 +1,6 @@
 <?php // phpcs:ignore Generic.Files.LineEndings.InvalidEOLChar
 
-add_filter( 'acf/settings/save_json', 'tcbp_public_json_save_point_1', 10, 1 );
+//add_filter( 'acf/settings/save_json', 'tcbp_public_json_save_point_1', 10, 1 );
 
 /**
  * Syncs the roster data from a JSON file located at the specified path.
@@ -9,7 +9,7 @@ function tcbp_public_json_save_point_1() {
 	return plugin_dir_path( __DIR__ ) . 'acf-json';
 }
 
-add_filter( 'acf/settings/load_json', 'tcbp_public_json_load_point', 10, 1 );
+//add_filter( 'acf/settings/load_json', 'tcbp_public_json_load_point', 10, 1 );
 
 /**
  * Loads the JSON data from the specified paths.
